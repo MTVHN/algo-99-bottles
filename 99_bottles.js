58606d5890810976e83d476a13dfab257f4c94c6
MTVHN <mathavhane@gmail.com>
2023-11-12 00:23:35 -0500

    add 99 bottles of beer method

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,18 @@
-function bottleSong() {
+function bottleSong(number) {
   // Write your code here!
+  for (let i = number; i > 0; i--){
+    
+    let bottles = i == 1 ? 'bottle' : 'bottles';
+    let bottle = i - 1 == 1 ? 'bottle' : 'bottles';
+
+    console.log(`${i} ${bottles} of beer on the wall, ${i} ${bottles} of beer.`);
+    console.log(`You take one down and pass it around, ${i - 1 == 0 ? 'no more': i - 1} ${bottle} of beer on the wall.`);
+    
+    if (i - 1 == 0){
+      console.log(`No more bottles of beer on the wall, no more bottles of beer.
+      Go to the store and buy some more, ${number} bottles of beer on the wall.`)
+    } 
+  }
 };
 
-bottleSong();
+bottleSong(4);
